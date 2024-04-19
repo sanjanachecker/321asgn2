794fba4d34a284cd802531eabd85ce2146dbf510
sanjanachecker <sanjanachecker@gmail.com>
2024-04-19 12:19:46 -0700

    cbc

---
+++ cbc.py
@@ -0,0 +1,57 @@
+from ssl import RAND_bytes
+import sys
+from Crypto.Cipher import AES
+
+
+def pad(data, block_size=16):
+    padding_len = block_size - len(data) % block_size
+    padding = bytes([padding_len] * padding_len)
+    return data + padding
+
+
+def unpad(data, block_size=16):
+    # print("data to unpad:", data)
+    # print("last byte:",data[-1])
+    padding_len = data[-1]
+    return data[:-padding_len]
+
+
+def cbc_decrypt(encrypted_string, key, iv):
+
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    decrypted_data = b''
+    prev_block = iv
+
+    for i in range(0, len(encrypted_string), 16):
+        encrypted_block = encrypted_string[i:i+16]
+        decrypted_block = cipher.decrypt(encrypted_block)
+        # xor with previous block after decrypting
+        decrypted_block = bytes(
+            x ^ y for x, y in zip(decrypted_block, prev_block))
+        decrypted_data += decrypted_block
+        prev_block = encrypted_block
+
+    decrypted_data = unpad(decrypted_data)
+    # print("decrypted data:", decrypted_data)
+
+    return decrypted_data
+
+
+def cbc_encrypt(im, key, iv):
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded_file = pad(im)
+
+    encrypted_data = bytes()
+    prev_block = iv
+
+    for i in range(0, len(padded_file), 16):
+        block = padded_file[i:i+16]
+        # xor with previous block before encrypting
+        block = bytes(x ^ y for x, y in zip(block, prev_block))
+        encrypted_block = cipher.encrypt(block)
+        encrypted_data += encrypted_block
+        prev_block = encrypted_block
+
+    return encrypted_data, key, iv
--- diffie.py
+++ diffie.py
@@ -1,5 +1,10 @@
 import hashlib
+from ssl import RAND_bytes
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from cbc import *
+
 
 q_hex = 'B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C013ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD7098488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708DF1FB2BC 2E4A4371'
 a_hex = 'A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5'
@@ -36,3 +41,13 @@ secret_key = str(k_a).encode('utf-8')
 
 k = hashlib.sha256(secret_key).hexdigest()[:16]
 print(k)
+
+
+iv = RAND_bytes(16)
+
+
+m1 = "hi bob".encode('utf-8')
+encrypted_m1 = cbc_encrypt(m1, iv, k)
+print("encrypted message ", encrypted_m1)
+m1_decrypted = cbc_decrypt(encrypted_m1, k, iv)
+print("decrypted ", m1_decrypted)
