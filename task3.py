9e3e26e78f39076e17d9ebe9bf4808f4cad49b04
sanjanachecker <sanjanachecker@gmail.com>
2024-04-22 14:14:06 -0700

    gen key

--- task3.py
+++ task3.py
@@ -17,7 +17,13 @@ print(q)
 
 def generateKey(p, q):
     n = p * q
-    on = (p-1) * (q-1)
+    phi_n = (p - 1) * (q - 1)
     e = 65537
 
-    d = 1/(e)
+    d = pow(e, -1, phi_n)
+    return ((n, e), (n, d))
+
+
+public_key, private_key = generateKey(p, q)
+print("Public key:", public_key)
+print("Private key:", private_key)
